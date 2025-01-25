a905b82b3ad838b2b9d526781802216d1b56518b
amir <amirjoun2@gmail.com>
2025-01-25 23:49:40 +0400

    Added some configs to global config

--- config/global.php
+++ config/global.php
@@ -7,9 +7,12 @@ return [
         ApiSources::cases(),
         function ($carry, $source) {
             $carry[$source->value] = [
-                'api_key' => env(strtoupper($source->value) . '_KEY') ?: 'test',//throw new InvalidArgumentException("API key for {$source->name()} is not set."),
+                'api_key' => env(strtoupper($source->value) . '_KEY') ?: 'test',
                 'base_url' => env(strtoupper($source->value) . '_BASE_URL', 'default_url'),
                 'timeout' => env(strtoupper($source->value) . '_TIMEOUT', 30),
+                'active' => env(strtoupper($source->value) . '_KEY', false) !== false,
+                'max_content' => 1024,
+                'days_ago' => 2, //default days ago to collect news
             ];
             return $carry;
         },
