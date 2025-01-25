4758495b8731c9a960670c0a77d2541d37ec8a20
amir <amirjoun2@gmail.com>
2025-01-26 00:14:51 +0400

    Added NewsAdapterFactory class to load each api sources adapters based on condition

---
+++ app/Services/Factories/NewsAdapterFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Factories;
+
+use App\Enums\ApiSources;
+use App\Contracts\ArticleAdapterInterface;
+use App\Services\Adapters\NewsApiOrgAdapter;
+use App\Services\Adapters\TheGuardianAdapter;
+use App\Services\Adapters\NewYorkTimesAdapter;
+
+class NewsAdapterFactory
+{
+    public static function make(string $apiSourceId): ArticleAdapterInterface
+    {
+        return match ($apiSourceId) {
+            ApiSources::NEWSAPIORG->value => new NewsApiOrgAdapter($apiSourceId),
+            ApiSources::THEGUARDIAN->value => new TheGuardianAdapter($apiSourceId),
+            ApiSources::NEWYORKTIMES->value => new NewYorkTimesAdapter($apiSourceId),
+            default => throw new \InvalidArgumentException("Unsupported API source: {$apiSourceId}"),
+        };
+    }
+}
