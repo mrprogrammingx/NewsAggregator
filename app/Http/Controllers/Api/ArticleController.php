504c42a23473ec26e91f624899111d5e03efb138
amir <amirjoun2@gmail.com>
2025-01-26 01:44:08 +0400

    Added ArticleController

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\ArticleSearchRequest;
+use App\Http\Resources\ArticlePaginationCollection;
+use App\Http\Resources\ArticleResource;
+use App\Http\Controllers\Controller;
+use App\Contracts\ArticleServiceInterface;
+use App\Http\Requests\ArticleStoreRequest;
+
+class ArticleController extends Controller
+{
+    public function __construct(public readonly ArticleServiceInterface $articleService)
+    {
+    }
+    public function index(): ArticlePaginationCollection
+    {
+        return new ArticlePaginationCollection($this->articleService->all());
+    }
+
+    public function store(ArticleStoreRequest $request): ArticleResource
+    {
+        return new ArticleResource($this->articleService->store($request->validated()));
+    }
+
+    public function fetchAllNewsApies(): array
+    {
+        $articles = $this->articleService->fetchAllNewsApies();
+        return $articles;
+    }
+
+    public function search(ArticleSearchRequest $request): ArticlePaginationCollection
+    {
+        return new ArticlePaginationCollection($this->articleService->search($request->validated()));
+    }
+}
