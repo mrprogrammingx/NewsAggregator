3cfdbfb02e47ee1c78844498840e5d2d073981d1
amir <amirjoun2@gmail.com>
2025-01-26 21:17:58 +0400

    Added for custom validation with form request

---
+++ app/Traits/ValidationTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Validator;
+
+trait ValidationTrait
+{
+    use ErrorLogTrait;
+
+    public function validateWithCustomFormRequest(array $data,\Illuminate\Foundation\Http\FormRequest $formRequest): array|bool
+    {
+        $validator = Validator::make(
+            $data, 
+            $formRequest->rules(), 
+            $formRequest->messages(), 
+            $formRequest->attributes()
+        );
+        
+        if ($validator->fails()) {
+            $this->logError('Failed in validate:', ['error' => $validator->errors(), 'data' => $data]);
+
+            return false;
+        }
+
+        return $validator->validated();
+    }
+}
