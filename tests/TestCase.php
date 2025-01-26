b4383dd356e69734f08b2ddb98374f2a1cc87fed
amir <amirjoun2@gmail.com>
2025-01-26 21:42:35 +0400

    Added setUp method for loading default instance of every interface

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\Repositories\ArticleRepository;
+use App\Contracts\ApiSourcesServiceInterface;
+use App\Contracts\ArticleRepositoryInterface;
+use App\Services\ApiSourcesService;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->instance(ArticleRepositoryInterface::class, resolve(ArticleRepository::class));
+        $this->app->instance(ApiSourcesServiceInterface::class, resolve(ApiSourcesService::class));
+    }
 }
