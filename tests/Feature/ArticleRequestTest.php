34db1402c5f4c4538150ec19bb009a06afc74016
amir <amirjoun2@gmail.com>
2025-01-26 21:44:37 +0400

    Added for testing Article Requests

---
+++ tests/Feature/ArticleRequestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Requests\ArticleStoreRequest;
+use Illuminate\Support\Facades\Validator;
+
+class ArticleRequestTest extends TestCase
+{
+    public function test_validation_rules_for_article_store_request()
+    {
+        $request = new ArticleStoreRequest();
+
+        $data = [
+            'title' => '',
+            'content' => '',
+            'category' => '',
+            'source' => '',
+            'author' => '',
+            'published_at' => 'invalid-date',
+        ];
+
+        $validator = Validator::make($data, $request->rules());
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('title', $validator->errors()->toArray());
+        $this->assertArrayHasKey('published_at', $validator->errors()->toArray());
+    }
+}
