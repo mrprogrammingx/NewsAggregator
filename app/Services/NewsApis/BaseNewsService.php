66a1515579b9926b00e4ac31f0a6b08bf3999683
amir <amirjoun2@gmail.com>
2025-02-27 22:15:26 +0400

    Added abstract class for NewsServices classes

---
+++ app/Services/NewsApis/BaseNewsService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\NewsApis;
+
+use App\Exceptions\AppException;
+use App\Traits\ErrorLogTrait;
+use App\Exceptions\ApiException;
+use Illuminate\Support\Facades\Http;
+use App\Contracts\NewsServicesInterface;
+
+abstract class BaseNewsService implements NewsServicesInterface
+{
+    use ErrorLogTrait;
+
+    protected string $apiSourceId;
+    protected array $apiSourceConfig;
+
+    public function callApi(string $path = '', array $queryParams = [], string $resultKey = ''): array
+    {
+        try {
+
+            $response = Http::get($this->apiSourceConfig['base_url'] . $path, $queryParams);
+
+            if ($response->failed()) {
+
+                $this->logError('Failed to fetch articles', [
+                    'status' => $response->status(),
+                    'body' => $response->body(),
+                    'source_api' => $this->apiSourceId,
+                ]);
+
+                throw new ApiException('Failed to fetch articles from API:' . $this->apiSourceId);
+            }
+
+            return (array) $response->json($resultKey);
+        } catch (AppException $e) {
+
+            $this->logError('Error fetching articles', ['message' => $e->getMessage(), 'source_api' => $this->apiSourceId]);
+
+            return [];
+        }
+    }
+}
